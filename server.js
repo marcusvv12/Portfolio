0458bb38c8cb1183f9fb4366eaa7260343916645
Marcus <marcus.vva12@gmail.com>
2025-08-15 18:37:04 -0300

    host

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const { createServer } = require('vite');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5173;
+
+app.use(express.static('dist'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
